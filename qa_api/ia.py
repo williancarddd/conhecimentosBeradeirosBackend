d8397f3f7679d69f343da5bf1ad5fb9f4e8a59fb
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-10-25 08:17:50 -0400

    update context.json

--- qa_api/ia.py
+++ qa_api/ia.py
@@ -3,6 +3,7 @@ from transformers import pipeline
 model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
 nlp = pipeline("question-answering", model=model_name)
 
+
 def question_answer(question, context):
     result = nlp(question=question, context=context)
     i = 0
@@ -10,12 +11,18 @@ def question_answer(question, context):
     answer = ""
 
     while context[result["start"] - i] != '.':
+        if i > 200:
+            i = result["start"]
+            break
         i += 1
 
     answer += context[result["start"] - i + 2:result["start"]]
     i = 0
 
     while context[result["start"] + i] != '.':
+        if i > 200:
+            i = len(context) - result["start"]
+            break
         i += 1
 
     answer += context[result["start"]:result["start"] + i]
--- qa_api/main.py
+++ qa_api/main.py
@@ -2,18 +2,24 @@ from transformers import pipeline
 import json
 from api_settings import app, cache
 from flask import request
-from ia import question_answer
 from unidecode import unidecode
 
 keywords = []
 
-with open('keywords.json') as file:
+keywords = []
+
+with open("context.json") as file:
     keywords = json.load(file)
+
 
 def get_context(question):
     for keyword in keywords:
-        if keyword in unidecode(question.lower()):
-            return keywords[keyword]["context"]
+        for item in keywords[keyword]["keywords"]:
+            if keyword in unidecode(question.lower()):
+                return keywords[item]["context"]
+            else:
+                return keywords["geral"]["context"]
+
 
 # Rota para fazer perguntas
 @app.route("/perguntar", methods=['POST'])
@@ -24,11 +30,18 @@ def ask_question():
         return json.dumps({"error": "A chave 'questions' está ausente nos dados enviados."}), 400
 
     questions = data["questions"]
+
     results = []
+
     for question in questions:
-        context = get_context(question)
+        if "context" in question:
+            context = question["context"]
+        else:
+            context = get_context(question)
         results.append(question_answer(question, context))
+
     return json.dumps(results)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
